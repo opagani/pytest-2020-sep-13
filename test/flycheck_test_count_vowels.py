af2a94828a41c16ff76131740c0f67d56a33dd90
Reuven Lerner <reuven@lerner.co.il>
2020-09-13 20:02:17 +0300

    gitautosync update 101

---
+++ test/flycheck_test_count_vowels.py
@@ -0,0 +1,17 @@
+import pytest
+
+from count_vowels import count_vowels
+
+
+@pytest.mark.parametrize('s, count',        # part 1: a string with comma-separated variable names
+                         [('', 0),          # part 2: a list of tuples; each tuple describes values assigned to the variables
+                          ('rhythm', 0),
+                          ('hello', 2),
+                          ('GOODBYE', 3)])
+def test_count_vowels(s, count):            # part 3: the variables are s
+    assert count_vowels(s) == count
+
+
+def test_list():
+    with pytest.raises(AttributeError):
+        count_vowels(['abcdefg'])
