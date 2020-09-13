ebb75025749b48f9db49c840df60bc4b550ba6e4
Reuven Lerner <reuven@lerner.co.il>
2020-09-13 20:08:18 +0300

    gitautosync update 105

--- test/test_count_vowels.py
+++ test/test_count_vowels.py
@@ -3,13 +3,12 @@ import pytest
 from count_vowels import count_vowels
 
 
-@pytest.mark.parametrize('s, count',        # part 1: a string with comma-separated variable names
-                         [('', 0),          # part 2: a list of tuples; each tuple describes values assigned to the variables
+@pytest.mark.parametrize('s, count',  # part 1: a string with comma-separated variable names
+                         [('', 0),  # part 2: a list of tuples; each tuple describes values assigned to the variables
                           ('rhythm', 0),
                           ('hello', 2),
                           ('GOODBYE', 3)])
-# part 3: the variables are parameters for the function
-def test_count_vowels(s, count):
+def test_count_vowels(s, count):  # part 3: the variables are parameters for the function
     # part 4: we use the variables (parameters) in our test
     assert count_vowels(s) == count
 
