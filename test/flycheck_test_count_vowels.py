8a8e7ed65d0d50ba1ef65dc09b0a9c70028bfeef
Reuven Lerner <reuven@lerner.co.il>
2020-09-13 20:01:48 +0300

    gitautosync update 99

---
+++ test/flycheck_test_count_vowels.py
@@ -0,0 +1,17 @@
+import pytest
+
+from count_vowels import count_vowels
+
+
+@pytest.mark.parametrize('s, count',        # part 1: a string with comma-sepa
+                         [('', 0),
+                          ('rhythm', 0),
+                          ('hello', 2),
+                          ('GOODBYE', 3)])
+def test_count_vowels(s, count):
+    assert count_vowels(s) == count
+
+
+def test_list():
+    with pytest.raises(AttributeError):
+        count_vowels(['abcdefg'])
