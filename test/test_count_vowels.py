19ed59a45dc9ffdfacf093667aac8e8793d4be20
Reuven Lerner <reuven@lerner.co.il>
2020-09-13 19:11:49 +0300

    gitautosync update 47

--- test/test_count_vowels.py
+++ test/test_count_vowels.py
@@ -1,1 +1,17 @@
 from count_vowels import count_vowels
+
+
+def test_count_empty():
+    assert count_vowels('') == 0
+
+
+def test_no_vowel_word():
+    assert count_vowels('rhythm') == 0
+
+
+def test_simple_word():
+    assert count_vowels('hello') == 2
+
+
+def test_simple_goodbye():
+    assert count_vowels('GOODBYE') == 3
