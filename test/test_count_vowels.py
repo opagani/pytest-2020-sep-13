31c53bcb80af3c025a4ea7f75d69846fb3b664a9
Reuven Lerner <reuven@lerner.co.il>
2020-09-13 19:48:18 +0300

    gitautosync update 77

--- test/test_count_vowels.py
+++ test/test_count_vowels.py
@@ -3,22 +3,22 @@ import pytest
 from count_vowels import count_vowels
 
 
-def test_count_empty():
+def test_count_empty() -> None:
     assert count_vowels('') == 0
 
 
-def test_no_vowel_word():
+def test_no_vowel_word() -> None:
     assert count_vowels('rhythm') == 0
 
 
-def test_simple_word():
+def test_simple_word() -> None:
     assert count_vowels('hello') == 2
 
 
-def test_simple_goodbye():
+def test_simple_goodbye() -> None:
     assert count_vowels('GOODBYE') == 3
 
 
-def test_list():
+def test_list() -> None:
     with pytest.raises(AttributeError):
         count_vowels(['abcdefg'])
