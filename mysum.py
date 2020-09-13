30beffd896dfd9911516292a27062f21ad2102da
Reuven Lerner <reuven@lerner.co.il>
2020-09-13 18:43:41 +0300

    gitautosync update 25

---
+++ mysum.py
@@ -0,0 +1,6 @@
+# write a function, mysum, that takes a sequence of numbers
+# and then returns the sum of those numbers.
+
+# - mysum.py
+# - test_mysum.py
+# - write a few functions that test different numbers/types
