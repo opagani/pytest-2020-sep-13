1d3cdb676c07ce872eb5b8e46c7e09528b87d9cd
Reuven Lerner <reuven@lerner.co.il>
2020-09-13 18:38:36 +0300

    gitautosync update 20

--- test_hello.py
+++ test_hello.py
@@ -4,3 +4,8 @@ from hello import hello
 def test_hello():
     output = hello('world')
     assert output == 'Hello, world!'
+
+
+def test_hello2():
+    output = hello('someone else')
+    assert output == 'Hello, someone else!'
