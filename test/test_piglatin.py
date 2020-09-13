e164f0af336059743363d4b62d76544d0c10c493
Reuven Lerner <reuven@lerner.co.il>
2020-09-13 22:00:27 +0300

    gitautosync update 67

--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -12,7 +12,12 @@ def test_plword(one_word, translation):
     assert plword(one_word) == translation
 
 
-def test_print_plword(capsys):
-    print_plword('hello')
+@pytest.mark.parametrize('one_word, translation',
+                         [('cat', 'atcay'),
+                          ('computer', 'omputercay'),
+                          ('ELEPHANT', 'elephantway'),
+                          ('octopus', 'octopusway')])
+def test_print_plword(capsys, one_word, translation):
+    print_plword(one_word)
     captured_out, captured_err = capsys.readouterr()
-    assert captured_out.strip() == 'hello is ellohay'
+    assert captured_out.strip() == f'{one_word} is {translation}'
