9a7ef3a5f6e0c42e5fd83607405c0157fc7b2877
Reuven Lerner <reuven@lerner.co.il>
2020-09-13 19:50:38 +0300

    gitautosync update 79

---
+++ test/flycheck_test_count_vowels.py
@@ -0,0 +1,24 @@
+import pytest
+
+from count_vowels import count_vowels
+
+
+def test_count_empty():
+    assert count_vowels('') == 0
+
+
+def test_no_vowel_word():
+    assert count_vowels('rhythm') == 0
+
+
+def test_simple_word():
+    assert count_vowels('hello') == 2
+
+
+def test_simple_goodbye():
+    assert count_vowels('GOODBYE') == 3
+
+
+def test_list():
+    with pytest.raises(AttributeError):
+        d count_vowels(['abcdefg'])
