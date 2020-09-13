40c6e180b889754b6653300ae6d5cc5f2df3fad4
Reuven Lerner <reuven@lerner.co.il>
2020-09-13 22:07:49 +0300

    gitautosync update 77

---
+++ test/flycheck_test_piglatin.py
@@ -0,0 +1,31 @@
+import pytest
+from io import StringIO
+
+from piglatin import plword, print_plword, print_interactive_plword
+
+
+@pytest.mark.parametrize('one_word, translation',
+                         [('cat', 'atcay'),
+                          ('computer', 'omputercay'),
+                          ('ELEPHANT', 'elephantway'),
+                          ('octopus', 'octopusway')])
+def test_plword(one_word, translation):
+    assert plword(one_word) == translation
+
+
+@pytest.mark.parametrize('one_word, translation',
+                         [('cat', 'atcay'),
+                          ('computer', 'omputercay'),
+                          ('ELEPHANT', 'elephantway'),
+                          ('octopus', 'octopusway')])
+def test_print_plword(capsys, one_word, translation):
+    print_plword(one_word)
+    captured_out, captured_err = capsys.readouterr()
+    assert captured_out.strip() == f'{one_word} is {translation}'
+
+
+def test_print_interactive_plword(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', StringIO('hello'))
+    print_interactive_plword()
+    captured_out, captured_err = capsys.readouterr()
+    assert captured_out.strip().endswith('hello is ellohay'
