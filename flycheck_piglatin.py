7755741b69b0416a422d0c2340c07b512003f861
Reuven Lerner <reuven@lerner.co.il>
2020-09-13 19:58:18 +0300

    gitautosync update 93

---
+++ flycheck_piglatin.py
@@ -0,0 +1,8 @@
+# Pig Latin -- children's "secret" language
+
+# - write a function that translates from English into Pig Latin
+# - (assume all lowercase, one word, no punctuation)
+
+# - write the function, plword
+# - takes one argument (string)
+# - returns one argument (string)
