8ce7803d4f99d19cc4a64c4e82ec4f54af3e1964
Reuven Lerner <reuven@lerner.co.il>
2020-09-13 18:50:51 +0300

    gitautosync update 27

--- mysum.py
+++ mysum.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+
+def mysum(numbers):
+    total = 0
+
+    for one_number in numbers:
+        total += one_number
+
+    return total
+
 # write a function, mysum, that takes a sequence of numbers
 # and then returns the sum of those numbers.
 
