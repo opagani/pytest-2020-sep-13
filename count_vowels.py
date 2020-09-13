bad0c8765dcf246ecceec6387a5e10392172eb39
Reuven Lerner <reuven@lerner.co.il>
2020-09-13 19:02:00 +0300

    gitautosync update 37

---
+++ count_vowels.py
@@ -0,0 +1,9 @@
+# write a function, count_vowels, which takes a string as
+# an argument and returns an integer count of how many
+# vowels were in the string
+
+# - create the file/function
+# - create the test file
+# - write a bunch of tests
+#   - empty string?
+#   - no vowels?
